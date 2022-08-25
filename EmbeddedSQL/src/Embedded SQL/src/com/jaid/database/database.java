c62601abf14f03cadb82ecd537a568c7b1e7b3ac
Jai <jai.dewani.99@gmail.com>
2022-08-26 01:46:26 +0530

    Update database interface

--- EmbeddedSQL/src/Embedded SQL/src/com/jaid/database/database.java
+++ EmbeddedSQL/src/Embedded SQL/src/com/jaid/database/database.java
@@ -1,4 +1,4 @@
-package com.jaid;
+package com.jaid.database;
 
 import java.io.*;
 import java.util.*;
@@ -6,26 +6,60 @@ import com.jaid.database.Selector;
 
 public interface Table extends Serializable, Cloneable
 {
-    Object clone();
+    // Return a shallow copy of the table
+    Object clone() throws CloneNotSupportedException;
 
+    // Return the table name
     String name();
 
+    // Rename the table to the indicated name
     void rename(String newName);
 
+    // Return true if this table has changed since it was last saved.
+    // The dirty bit is cleared when you export the table
     boolean isDirty();
 
+    /** Insert new values into the table corresponding to the
+     * specified column names. For example, the value at
+     * <code>values[i]</code> is put into the column specified in
+     * <code>columnNames[i]</code>
+     * @param columnNames
+     * @param values
+     * @return the number of rows affectd by the operation
+     * @throws IndexOutOfBoundsException One of the requested column doesn't exist in either table
+     */
     int insert(String[] columnNames, Object[] values);
 
+    /**A convenience overload of {@link #insert(String[], Object[])} */
     int insert(Collection columnNames, Collection values);
 
+    /**
+     * In this insert, values must have as many elements as there are columns,
+     * and in the same order of columns
+     * @param values
+     * @return the number of rows affected by the operation
+     */
     int insert(Object[] values);
 
+    /** A convenience overload of {@link #insert(Object[])} */
     int insert(Collection values);
 
+    /** Update the cells in the table. The {@link Selector} object server
+     * as a visitor whose <code>includeInSelect(...)</code> method
+     * is called for each row in the table.
+     * @param where
+     * @return the number of rows affected by the operation.
+     */
     int update(Selector where);
 
+    /**
+     * Delete from the table all rows approved by the Selector.
+     * @param where
+     * @return the number of rows affected by the operation
+     */
     int delete(Selector where);
 
+    /** begin a transaction */
     public void begin();
 
     public void commit(boolean all) throws IllegalStateException;
@@ -53,6 +87,23 @@ public interface Table extends Serializable, Cloneable
 
     public interface Exporter
     {
-        public void startTable() throws  
+        public void startTable() throws  IOException;
+        public void storeMetadata(
+                String tableName,
+                int width,
+                int height,
+                Iterator columnNames) throws IOException;
+        public void storeRow(Iterator data) throws IOException;
+        public void endTable() throws IOException;
+    }
+
+    public interface Importer
+    {
+        void startTable() throws IOException;
+        String loadTableName() throws IOException;
+        int loadWidth() throws IOException;
+        Iterator loadColumnNames() throws IOException;
+        Iterator loadRow() throws IOException;
+        void endTable() throws IOException;
     }
 }
